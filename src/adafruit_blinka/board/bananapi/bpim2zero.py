c82ab37809b66b9ece4061225dbe82e81384d645
Mike McMahon <mmcm@comcast.net>
2023-06-17 18:20:45 -0700

    Clean up BPi M2 Zero a little. There were duplicate identical assignments due to merge skew. Add Dnn numbering as well, compatible with some other boards (though not RPi itself).

--- src/adafruit_blinka/board/bananapi/bpim2zero.py
+++ src/adafruit_blinka/board/bananapi/bpim2zero.py
@@ -3,80 +3,86 @@
 # SPDX-License-Identifier: MIT
 """Pin definitions for the Banana Pi M2 Zero."""
 
-# The Banana Pi M2 Zero uses the AllWinner H2 SoC, but pins
-# are the same as the AllWinner H3 SoC, so we import those
+# The Banana Pi M2 Zero now uses the AllWinner H3 SoC;
+# older models used H2+, but pins are the same
 from adafruit_blinka.microcontroller.allwinner.h3 import pin
 
 PA12 = pin.PA12
+D3 = pin.PA12
 SDA = pin.PA12
 PA11 = pin.PA11
+D5 = pin.PA11
 SCL = pin.PA11
 PA6 = pin.PA6
+D7 = pin.PA6
 PWM1 = pin.PA6
 PA1 = pin.PA1
+D11 = pin.PA1
 UART2_RX = pin.PA1
 PA0 = pin.PA0
+D13 = pin.PA0
 UART2_TX = pin.PA0
 PA3 = pin.PA3
+D15 = pin.PA3
 UART2_CTS = pin.PA3
-PA10 = pin.PA10
-PA12 = pin.PA12
-SDA = pin.PA12
-PA11 = pin.PA11
-SCL = pin.PA11
-PA6 = pin.PA6
-PWM1 = pin.PA6
-PA1 = pin.PA1
-UART2_RX = pin.PA1
-PA0 = pin.PA0
-UART2_TX = pin.PA0
-PA3 = pin.PA3
-UART2_CTS = pin.PA3
-PA7 = pin.PA7
-PA8 = pin.PA8
-PA9 = pin.PA9
-PA10 = pin.PA10
-PA17 = pin.PA17
-PA18 = pin.PA18
-PA19 = pin.PA19
-PA20 = pin.PA20
-PA21 = pin.PA21
 PC0 = pin.PC0
+D19 = pin.PC0
+MOSI = pin.PC0
 PC1 = pin.PC1
+D21 = pin.PC1
+MISO = pin.PC1
 PC2 = pin.PC2
-PC3 = pin.PC3
+D23 = pin.PC2
+SCLK = pin.PC2
+PA19 = pin.PA19
+D27 = pin.PA19
+TWI1_SDA = pin.PA19
+PA7 = pin.PA7
+D29 = pin.PA7
+PA8 = pin.PA8
+D31 = pin.PA8
+PA9 = pin.PA9
+D33 = pin.PA9
+PA10 = pin.PA10
+D35 = pin.PA10
+PA17 = pin.PA17
+D37 = pin.PA17
+
+PA13 = pin.PA13
+D8 = pin.PA13
+SPI1_CS = pin.PA13
+PA14 = pin.PA14
+D10 = pin.PA14
+SPI1_CLK = pin.PA14
+PA16 = pin.PA16
+D12 = pin.PA16
+SPI1_MISO = pin.PA16
+PA15 = pin.PA15
+D16 = pin.PA15
+SPI1_MOSI = pin.PA15
 PC4 = pin.PC4
+D18 = pin.PC4
+PA2 = pin.PA2
+D22 = pin.PA2
+UART2_RTS = pin.PA2
+PC3 = pin.PC3
+D24 = pin.PC3
+CS = pin.PC3
 PC7 = pin.PC7
-
-PA13 = pin.PA13
-SPI1_CS = pin.PA13
-PA14 = pin.PA14
-SPI1_CLK = pin.PA14
-PA2 = pin.PA2
-UART2_RTS = pin.PA2
+D26 = pin.PC7
 PA18 = pin.PA18
+D28 = pin.PA18
 TWI1_SCK = pin.PA18
-
 PL2 = pin.PL2
+D32 = pin.PL2
 PL4 = pin.PL4
+D36 = pin.PL4
+PA21 = pin.PA21
+D38 = pin.PA21
+PA20 = pin.PA20
+D40 = pin.PA20
 
-SCLK = pin.PA14
-MOSI = pin.PA15
-MISO = pin.PA16
-
-PA13 = pin.PA13
-SPI1_CS = pin.PA13
-PA14 = pin.PA14
-SPI1_CLK = pin.PA14
-PA2 = pin.PA2
-UART2_RTS = pin.PA2
-PA18 = pin.PA18
-TWI1_SCK = pin.PA18
 PG6 = pin.PG6
 UART1_TX = pin.PG6
 PG7 = pin.PG7
 UART1_RX = pin.PG7
-
-SCLK = pin.PA14
-MOSI = pin.PA15
-MISO = pin.PA16
